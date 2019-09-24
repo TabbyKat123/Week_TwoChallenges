10d146bf692d6c52fae1cec9668325719945037c
TabbyKat123 <btabbynamwone@gmail.com>
2019-09-24 12:49:48 +0300

    Age checker program

---
+++ agechecker.js
@@ -0,0 +1,7 @@
+var agePrompt=prompt("How old are you?");
+if (agePrompt>=18)
+alert("Have a beer, You are an adult!")
+else if(agePrompt>35)
+alert("Have a beer too. You are an elder.")
+else(agePrompt<18)
+alert("You are underage. Have a sweet.")
